8a8887c7021d68d4f6a7e7a7d403500e17a84235
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-08-16 17:05:19 +0000

    initial commit.

---
+++ variables.py
@@ -0,0 +1,10 @@
+message = "hello python world!u575"
+print(message)
+
+message = "Pyhton is my favorite language!"
+print(message)
+message = "Thank you for sharing Python with the world, Guido!"
+print(message)
+message = 'Thankyou for sharing Python world, Guido!'
+print(message)
+
