bf43cb6c30e04cc125c297f020d4dcc8578d693a
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-08-19 17:07:25 +0000

    Update

--- variables.py
+++ variables.py
@@ -7,4 +7,16 @@ message = "Thank you for sharing Python with the world, Guido!"
 print(message)
 message = 'Thankyou for sharing Python world, Guido!'
 print(message)
+My_string = "This is a double-quoted string."
+My_string = "This is a double-quoted string."
+quote = "Linusn Torvalds once said,'Any program is only as good as it is useful"
+first_name = 'eric'
+print(first_name)
+print(first_name.title())
+first_name = 'eric'
+print(first_name)
+print(first_name.title())
+print(first_name.upper())
+first_name = 'Eric'
+print(first_name.lower())
 
