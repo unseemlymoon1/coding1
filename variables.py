a3c365c8c450ea12701f141c679b758791e7fe44
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-08-30 16:05:08 +0000

    update commit.

--- variables.py
+++ variables.py
@@ -21,10 +21,6 @@ first_name = 'Eric'
 print(first_name.lower())
 first_name = 'ada'
 last_name = 'love lace' 
-full_name = + ' ' + last_name
-print(full_name.title())
-message = full_name.title() + ' ' + "was considered the world's first computer programmer."
-print(message)
 print("Hello everyone!")
 print("\tHello everyone!")
 print ("h\Hello \teveryone!")
@@ -34,7 +30,26 @@ print("\nHello everyone!")
 print(message)
 print("\n\n\nHello everyone!")
 name = ' eric '
-
 print(name.lstrip())
-
+print(name.rstrip())
+print(name.strip())
+name = ' eric '
+print('-' + name.lstrip() + '-')
+print('-' + name.rstrip() + '-')
+print('-' + name.strip() + '-')
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3**2)
+standard_order = 2+3*4
+print(standard_order)
+my_order = (2+3)*4
+print(my_order)
+print(0.1+0.1)
+print(0.1+0.2)
+print(3*0.1)
+# I learned how to strip whitespace from strings.
+name = '\t\teric'
+print("I can strip tabs from my name: " + name.strip())
 
