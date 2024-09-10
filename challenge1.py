d51691781472bcf862b713e837db14fb1e23f281
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-09-10 17:08:57 +0000

    updare commit.

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,4 @@
+
 # I learned how to strip whitespace from strings.
 name = '\t\teric'
 #  i store this message and when i say print message it will print hwatever i have stored  
@@ -6,4 +7,22 @@ print(message)
 #when i store name and then press print name upper it will print name in upper case 
 first_name = 'jaden '
 print(first_name.upper())
-
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[0]
+dog = dogs[0]
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[1]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-1]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-2]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-4]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+for dog in dogs :
+ print(dog)
+ 
