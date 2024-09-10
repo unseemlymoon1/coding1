de26a7dedebaae30372358bfcbe582d027f98b4d
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-09-10 17:07:16 +0000

    update commit.

---
+++ challenge1.py
@@ -0,0 +1,9 @@
+# I learned how to strip whitespace from strings.
+name = '\t\teric'
+#  i store this message and when i say print message it will print hwatever i have stored  
+message = " i am jaden stanly "
+print(message)
+#when i store name and then press print name upper it will print name in upper case 
+first_name = 'jaden '
+print(first_name.upper())
+
