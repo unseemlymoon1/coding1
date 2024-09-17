e4b18bd54297337c625a5d2d5698c14e79e85c1d
unseemlymoon1 <jaden.stanley@student.oneidaschools.org>
2024-09-17 17:06:51 +0000

    final commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,24 @@
 print ('hello python world ')
+message = 'my name is Jaden'
+message = 'i am a highschooler'
+message = 'im a senior'
+message = 'hello python world my name is Jaden i am a highschooler im a senior'
+print(message)
+print ('Hello Python World ')
+'Hello Python World My Name Is Jaden I Am A Highschooler Im A Senior'
+print(message)
+print ('Hello Python World ')
+'HELLO PYTHON WORLD MY NAME IS JADEN I AM A HIGHSCHOOLER IM A SENIOR'
+print (message)
+'hello python world my name is jaden i am a highschooler im a senior'
+numbers = [4,5,6,6,7,7,8 ]
+print(numbers)
+print(4+5+6+6+7+7+8)
+numbers = 43
+print(numbers)
+print(43/7)
+numbers = 6.1
+
+
+
+
